5ecbb406bf85922065df12d2c2dde1377bb748cf
dylan-kerr <dylan-kerr@users.noreply.github.com>
2020-04-09 20:03:55 +0100

    Minor version bump and CHANGELOG update.

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -11,7 +11,7 @@ use Laminas\Session\SessionManager;
 
 class Module
 {
-    public const VERSION = '2.4.0';
+    public const VERSION = '2.5.0';
 
     public function onBootstrap(MvcEvent $e)
     {
