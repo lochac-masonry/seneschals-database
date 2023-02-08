d757db481c01d7dcdf5d0b0484437b94717592fe
dylan-kerr <dylan-kerr@users.noreply.github.com>
2023-02-08 09:01:35 +0100

    Test view rendering outside MVC loop

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 
 namespace Application\Controller;
 
+use Laminas\View\Model\ViewModel;
+
 class IndexController extends DatabaseController
 {
     public function indexAction()
     {
+        $view = new ViewModel([
+            'foo' => 'bar',
+        ]);
+        $view->setTemplate('event/email/announce.phtml');
+        $view->setTerminal(true);
+        $serviceManager = $this->getServiceLocator();
+        echo($serviceManager->get('ViewRenderer')->render($view));
+
         $this->layout()->title = 'Lochac Seneschals\' Database';
         return [
             'authLevel' => $this->auth()->getLevel(),
