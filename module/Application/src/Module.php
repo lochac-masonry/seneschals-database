0bea32d833abaf545e017cc98dca085936c72d2c
dylan-kerr <dylan-kerr@users.noreply.github.com>
2024-06-03 13:25:27 +0200

    Update title of Submit Event Proposal page to Submit Lochac Event for Approval (#20)

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -11,7 +11,7 @@ use Laminas\Session\SessionManager;
 
 class Module
 {
-    public const VERSION = '2.20.2';
+    public const VERSION = '2.20.3';
 
     public function onBootstrap(MvcEvent $e)
     {
