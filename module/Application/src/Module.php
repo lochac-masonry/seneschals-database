9af0ebe445e81a1e6216af7a86e54a9f8aac40c7
dylan-kerr <dylan-kerr@users.noreply.github.com>
2025-08-11 10:16:41 +0200

    Increase NZ event income threshold for insurer notification to $10,000 (#23)

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -11,7 +11,7 @@ use Laminas\Session\SessionManager;
 
 class Module
 {
-    public const VERSION = '2.21.1';
+    public const VERSION = '2.21.2';
 
     public function onBootstrap(MvcEvent $e)
     {
